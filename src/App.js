6755e21e6e01d25938bc3423b81a250c26766095
Maria Matthews <maria.matthews@thameswater.co.uk>
2021-05-11 00:45:26 +0100

    test 6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Terminal from 'terminal-in-react';
+import React, { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Terminal
+          color='red'
+          backgroundColor='black'
+          barColor='black'
+          hideTopBar='true'
+          allowTabs='false'
+          startState='maximised'
+          style={{ fontWeight: "bold", fontSize: "1em" }}
+          commands={{
+            'open-linkedin': () => window.open('https://www.linkedin.com/in/mariamatthews89/', '_blank'),
+            showmsg: this.showMsg,
+            popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'open-google': 'opens google.com',
+            showmsg: 'shows a message',
+            alert: 'alert', popup: 'alert'
+          }}
+          msg='Because why not lol. Welcome to matrix. Type "help" for the list of commands.'
+        />
     </div>
   );
 }
