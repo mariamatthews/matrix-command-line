cce3e3d1fc0a4c03ffcbc249cb6f6404226da7ee
Maria Matthews <maria.matthews@thameswater.co.uk>
2021-05-11 00:00:35 +0100

    pushing first console update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Terminal from 'terminal-in-react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        Oh well, here we are again:
+        <Terminal
+          color='green'
+          backgroundColor='black'
+          barColor='black'
+          style={{ fontWeight: "bold", fontSize: "1em" }}
+          commands={{
+            'open-google': () => window.open('https://www.google.com/', '_blank'),
+            showmsg: this.showMsg,
+            popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'open-google': 'opens google.com',
+            showmsg: 'shows a message',
+            alert: 'alert', popup: 'alert'
+          }}
+          msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+        />
+        
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +37,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
